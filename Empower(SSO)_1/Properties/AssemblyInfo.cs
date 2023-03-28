9b89a70c15108a41171711a02ec1384e70a705c7
stevensoenens <34860758+stevensoenens@users.noreply.github.com>
2023-03-28 14:08:24 +0200

    Add project files.

---
+++ Empower(SSO)_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Empower(SSO)_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("Empower(SSO)_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("A5D142F4-8A56-4C2B-80A4-EE4A1541BEB2")]
+[assembly: AssemblyVersion("1.0.0.0")]
